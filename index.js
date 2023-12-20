df2fff08d09122ac88081a615aa28a93dbe3c7c1
juice789 <qbyqby@gmail.com>
2023-12-20 15:42:40 +0100

    options update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+const EventEmitter = require('events')
+const Request = require('request')
 const SteamUser = require("steam-user")
 const Steamcommunity = require("steamcommunity")
 const Manager = require("steam-tradeoffer-manager")
 const Tf2 = require('tf2')
-const { compose } = require('ramda')
+const { compose, omit, clone } = require('ramda')
 const { getInstance } = require('./getInstance.js')
-
-const EventEmitter = require('events')
 
 var MyBot = function MyBot(steam, options) {
     EventEmitter.call(this)
@@ -17,13 +17,24 @@ MyBot.prototype = Object.create(EventEmitter.prototype)
 MyBot.prototype.constructor = MyBot
 
 const initSteam = (options) => {
-    const client = new SteamUser({ "promptSteamGuardCode": false })
-    const community = new Steamcommunity()
+    const client = new SteamUser({
+        promptSteamGuardCode: false,
+        ...options.client
+    })
+    const communityOptions = clone(options.community)
+    if (options.community?.request) {
+        communityOptions.request = Request.defaults({
+            forever: true,
+            ...options.community.request
+        })
+    }
+    const community = new Steamcommunity(communityOptions)
     const manager = new Manager({
-        "steam": client,
-        "community": community,
-        "domain": "localhost",
-        "language": "en"
+        steam: client,
+        community: community,
+        domain: "localhost",
+        language: "en",
+        ...options.manager
     })
     let tf2
     if (options.tf2) {
@@ -35,7 +46,7 @@ const initSteam = (options) => {
         manager,
         tf2
     }
-    return new MyBot(steam, options)
+    return new MyBot(steam, omit(['client, community, manager'], options))
 }
 
 const createBot = compose(getInstance, initSteam)
